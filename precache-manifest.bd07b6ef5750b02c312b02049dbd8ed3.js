ff8ffaf560a0818c3005f0c2347cccf40cc737d4
lilydao212 <chieuanh212@gmail.com>
2025-05-21 23:40:01 -0400

    Updates

---
+++ precache-manifest.bd07b6ef5750b02c312b02049dbd8ed3.js
@@ -0,0 +1,26 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "c07dc881e27813ed52946b26046be1c1",
+    "url": "/gf/index.html"
+  },
+  {
+    "revision": "8caf6cf14ce73e7a257f",
+    "url": "/gf/static/css/main.27244ffd.chunk.css"
+  },
+  {
+    "revision": "b1a192c5773b59dc6506",
+    "url": "/gf/static/js/2.0fb38b82.chunk.js"
+  },
+  {
+    "revision": "401cd9cda943463bea21",
+    "url": "/gf/static/js/3.261a8d0b.chunk.js"
+  },
+  {
+    "revision": "8caf6cf14ce73e7a257f",
+    "url": "/gf/static/js/main.03c2856e.chunk.js"
+  },
+  {
+    "revision": "2e08a553f8266e13f07a",
+    "url": "/gf/static/js/runtime~main.335edb5b.js"
+  }
+]);
